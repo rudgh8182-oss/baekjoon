e85bef4d17f0ac49ca3fb8c2180219f2505ad09e
rudgh8182@gmail.com <rudgh8182@gmail.com>
2026-02-26 17:29:55 +0900

    260226_1

--- 3980.py
+++ 3980.py
@@ -4,10 +4,10 @@ for _ in range(T):
     arr = [list(map(int, input().split())) for _ in range(11)]
     visited = [0] * 11
     
-    max_r_v = 0
+    max_v = 0
     
     def dfs(r):
-        global max_r_v
+        global max_v
         if r == 11:
             total = sum(visited)
             if max_v < total:
@@ -15,11 +15,14 @@ for _ in range(T):
             return
         
         for c in range(11):
-            if arr[r][c] > 0:
-                visited[r] = arr[r][c]
+            if visited[c] == 0:
+                if arr[r][c] > 0:
+                    visited[c] = arr[r][c]
                 
-                dfs(r + 1)
+                    dfs(r + 1)
 
-                visited[r] = 0
+                    visited[c] = 0
     
     dfs(0)
+
+    print(max_v)
