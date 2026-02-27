396ad080ee7d2c22fa1f81d3126992b185c33ac7
rudgh8182@gmail.com <rudgh8182@gmail.com>
2026-02-27 11:36:37 +0900

    260227-1

--- 1759.py
+++ 1759.py
@@ -2,5 +2,23 @@ L, C = map(int, input().split())
 word = list(input().split())
 word.sort()
 
-def dfs(r):
-    pass
+visited = [0] * L
+
+def dfs(r, start):
+    if r == L:
+        cnt = 0
+        for j in range(L):
+            if visited[j] in ['a', 'e', 'i', 'o', 'u']:
+                cnt += 1
+        if 1<= cnt <= L-2:
+            print(''.join(visited))
+        return
+    
+    for i in range(start, C):
+        visited[r] = word[i]
+
+        dfs(r + 1, i + 1)
+
+        visited[r] = word[i]
+
+dfs(0, 0)
